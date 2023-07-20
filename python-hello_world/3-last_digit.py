82e247b39feabab7cbdb9a9faef05853fdd77979
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-20 13:19:01 +0300

    updated

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,15 +1,8 @@
 import random
-
 number = random.randint(-10000, 10000)
-
-# Calculate the last digit of the number
 last_digit = abs(number) % 10
-# Ensure the last digit has the correct sign for negative numbers
 last_digit *= -1 if number < 0 else 1
-
-# Print the result
 print(f"Last digit of {number} is {last_digit}", end=' ')
-
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
