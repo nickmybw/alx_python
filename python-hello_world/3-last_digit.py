c7771be5e85887e56be57fe8de5fa67b179adc5c
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-20 12:51:12 +0300

    Updated commit 3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,16 +1,53 @@
 import random
 
-number = random.randint(-10000, 10000)
 
-# Get the last digit of the number
-last_digit = abs(number) % 10
+def get_last_digit(number):
+    return abs(number) % 10
 
-# Print the result based on the last digit
-print("Last digit of", number, "is", last_digit, end=' ')
 
+# Positive case
+number = random.randint(1, 10000)
+last_digit = get_last_digit(number)
+print(f"Last digit of {number} is {last_digit}", end=' ')
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
+
+# Negative case
+number = random.randint(-10000, -1)
+last_digit = get_last_digit(number)
+print(f"Last digit of {number} is {last_digit}", end=' ')
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
+
+# Zero case
+number = 0
+last_digit = get_last_digit(number)
+print(f"Last digit of {number} is {last_digit}", end=' ')
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
+
+# Wrong type case
+try:
+    number = "invalid"
+    last_digit = get_last_digit(number)
+    print(f"Last digit of {number} is {last_digit}", end=' ')
+    if last_digit > 5:
+        print("and is greater than 5")
+    elif last_digit == 0:
+        print("and is 0")
+    else:
+        print("and is less than 6 and not 0")
+except TypeError as e:
+    print("TypeError")
