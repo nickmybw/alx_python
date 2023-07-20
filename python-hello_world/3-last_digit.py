beef1b33314b6637a0d05771932f24e7ad66512d
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-20 12:24:48 +0300

    Updated commit 2

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 # Get the last digit of the number
-last_digit = number % 10
+last_digit = abs(number) % 10
 
 # Print the result based on the last digit
 print("Last digit of", number, "is", last_digit, end=' ')
