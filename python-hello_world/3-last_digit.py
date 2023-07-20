c0fef5f36a5ae1f93bbd7bdd45f599de8a715707
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-20 21:04:20 +0300

    Commit 1

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,6 +1,6 @@
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+last_digit = abs(number) % 10
 last_digit *= -1 if number < 0 else 1
 print(f"Last digit of {number} is {last_digit}", end=' ')
 if last_digit > 5:
