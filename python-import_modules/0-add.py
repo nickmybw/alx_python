7efe6bd743043e9e245b81db65f101557894ff00
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-28 15:42:58 +0300

    New commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,16 +1,12 @@
-if __name__ == "__main__":
-    a = 1
-    b = 2
+# Importing the add function from add_0.py
+from add_0 import add
 
-    # Import the 'add' function from 'add_0.py'
-    from add_0 import add
+# Define variables a and b
+a = 1
+b = 2
 
-    # Calculate the result using the 'add' function and print the output using string formatting
-    print(
-        f"Correct output - case: a = {a} and b = {b} FAKE add() => {a} - {b}")
-    print(f"Correct output - case: a = 10 and b = 30 FAKE add() => {a} - {b}")
-    print(f"Correct output - case: a = -10 and b = 30 FAKE add() => {a} - {b}")
-    print(
-        f"Correct output - case: a = -10 and b = -30 FAKE add() => {a} - {b}")
-    print(
-        f"Correct output - case: a = 5 and b = \"H\" FAKE add() => {a} - {b}")
+# Call the add function with variables a and b as arguments
+result = add(a, b)
+
+# Print the result using string formatting
+print(f"{a} + {b} = {result}")
