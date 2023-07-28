66373fcf84937e0d7ec5b81ea3bdf73f9a06667b
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-28 16:23:05 +0300

    first commit

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1: {}".format(args[0]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i, arg in enumerate(args):
+            print("{}: {}".format(i+1, arg))
