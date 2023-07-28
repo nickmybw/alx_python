beafa631dabba82659342eb8dcc0b2607325c40d
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-28 15:28:22 +0300

    first commit

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+from add_0 import add
+a = 1
+b = 2
+
+# Import the 'add' function from 'add_0.py'
+
+# Calculate the result using the 'add' function and print the output using string formatting
+print(f"{a} + {b} = {add(a, b)}")
