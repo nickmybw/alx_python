f7d3242ca8d435242872b7e0f1d41eb9250d2784
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-28 16:37:43 +0300

    commit

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,9 +1,6 @@
 if __name__ == "__main__":
     try:
-        # Import the variable 'a' from variable_load_2.py
-        from variable_load_2 import a
-
-        # Print the value of the variable 'a'
-        print("Correct output - case: a =", a)
-    except ImportError:
-        print("Correct output - case: a missing")
+        import variable_load_2
+        print(variable_load_2.a)
+    except AttributeError:
+        print("a missing")
