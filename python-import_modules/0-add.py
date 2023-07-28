457c772d4f3bed9c3feb6ff7bd171fa5bf53518a
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-28 15:57:39 +0300

    update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,12 @@
-# Import the add function from add_0.py
+# Importing the add function from add_0.py
 from add_0 import add
 
 # Define variables a and b
 a = 1
 b = 2
 
-# Call the add function with the defined variables a and b
+# Calculate the result using the add function
 result = add(a, b)
 
-# Print the result using string formatting
-print("{} + {} = {}".format(a, b, result))
+# Print the formatted output
+print(f"{a} + {b} = {result}")
