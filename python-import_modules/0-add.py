cd65a6334403f7e4ef51369196ecfc626f65d9b7
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-28 15:53:38 +0300

    updated

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3
+# Import the add function from add_0.py
 from add_0 import add
 
-if __name__ == "__main__":
-    a = 1
-    b = 2
+# Define variables a and b
+a = 1
+b = 2
 
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+# Call the add function with the defined variables a and b
+result = add(a, b)
+
+# Print the result using string formatting
+print("{} + {} = {}".format(a, b, result))
