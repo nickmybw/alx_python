04fafcad97b9dce3aa253d3c7bd46a6a0053925f
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-28 16:17:06 +0300

    Updated

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -14,7 +14,5 @@ def add(a, b):
 if __name__ == "__main__":
     a = 1
     b = 2
-
-    result = add(a, b)
-
-    print("{} + {} = {}".format(a, b, result))
+    from add_0 import add
+    print("{} + {} = {}".format(a, b, add(a, b)))
