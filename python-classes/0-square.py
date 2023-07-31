91945040b10816f1e793a8060f3d0b51ba4f3ab2
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-31 07:26:16 +0300

    updated

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,21 +1,3 @@
 class Square:
     def __init__(self, size):
         self.__size = size
-
-
-if __name__ == "__main__":
-    my_square = Square(3)
-    print(type(my_square))
-    print(my_square.__dict__)
-
-    try:
-        # Attempting to access the private attribute directly
-        print(my_square.size)
-    except Exception as e:
-        print(e)  # Output: 'Square' object has no attribute 'size'
-
-    try:
-        # Attempting to access the private attribute directly
-        print(my_square.__size)
-    except Exception as e:
-        print(e)  # Output: 'Square' object has no attribute '__size'
