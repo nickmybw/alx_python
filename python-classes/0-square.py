d47f505582153905ce651131e047baca3ca7aff8
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-07-31 07:35:08 +0300

    updated commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,20 +2,22 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    @property
     def dict_(self):
-        return {'size': self.__size}
+        return {"size": self.__size}
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.dict_())
 
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
+    try:
+        # Attempting to access the private attribute directly
+        print(my_square.size)
+    except Exception as e:
+        print(e)  # Output: 'Square' object has no attribute 'size'
 
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+    try:
+        # Attempting to access the private attribute using name-mangled access
+        print(my_square._Square__size)
+    except Exception as e:
+        print(e)  # Output: 'Square' object has no attribute '_Square__size'
