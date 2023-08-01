eb31c4d113aa7b3224d2fa13024a4183ae6d7b82
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-01 23:17:51 +0300

    update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,31 +1,28 @@
 class Square:
     def __init__(self, size=0):
-        self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = size
 
     def area(self):
         return self.__size ** 2
 
 
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
 
 try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    print(my_square_1.size)
 except Exception as e:
     print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
