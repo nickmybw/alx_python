0d1b0e30bcef3ee409165ab4ec7ebf9e90e4063d
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-01 22:32:42 +0300

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 class Square:
+    """Represents a square."""
+
     def __init__(self, size):
+        """Initialize a square with the given size."""
         self.__size = size
+
+    def area(self):
+        """Compute the area of the square."""
+        return self.__size ** 2
