81762b441a40f60d912d4f8c382b9ff216072a11
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-01 22:26:54 +0300

    first commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,26 +1,3 @@
 class Square:
     def __init__(self, size):
         self.__size = size
-
-    def area(self):
-        return self.__size ** 2
-
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
-
-print(my_square.area())
-
-my_square.__size = 5
-print(my_square.area())
