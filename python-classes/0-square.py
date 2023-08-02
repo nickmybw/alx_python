55493e6804feb98b2d7f8fceaee9eda517a72a22
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-02 19:23:00 +0300

    new commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -9,34 +9,17 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    def dict_(self):
-        """
-        Returns the dictionary representation of the square.
 
-        Returns:
-            dict: The dictionary representation of the square.
-        """
-        return {'size': self.__size}
-
-
-# Test case 1
 my_square = Square(3)
 print(type(my_square))  # <class '__main__.Square'>
-print(my_square.dict_())  # {'size': 3}
+print(my_square.__dict__)  # {'_Square__size': 3}
 
-# Test case 2
-my_square = Square(89)
-print(type(my_square))  # <class '__main__.Square'>
-print(my_square.dict_())  # {'size': 89}
-
-# Test case 3
 try:
     print(my_square.size)
-except AttributeError as e:
+except Exception as e:
     print(e)  # 'Square' object has no attribute 'size'
 
-# Test case 4
 try:
-    print(my_square._size)
-except AttributeError as e:
-    print(e)  # 'Square' object has no attribute '_size'
+    print(my_square.__size)
+except Exception as e:
+    print(e)  # 'Square' object has no attribute '__size'
