6a8de88b94bbf368892ac5c01231bc5236a55d62
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-02 18:48:42 +0300

    updated commit 1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -14,3 +14,27 @@ class Square:
             size (int): The size of the square.
         """
         self.__size = size
+
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    mysquare = Square(3)
+    print(type(mysquare))
+    print(mysquare.__dict__)
+
+    mysquare = Square(89)
+    print(type(mysquare))
+    print(mysquare.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(mysquare._size)
+    except Exception as e:
+        print(e)
