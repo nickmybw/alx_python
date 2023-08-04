54b774dbc13c289ab42ebaccaebe6f90cd443abe
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-04 18:12:44 +0300

    update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,10 +1,10 @@
 """ALX task 3"""
-
 class BaseGeometry:
-    pass
     """
     An empty class representing a base geometry.
 
     This class can be used as a base for more specific geometry classes.
     It currently does not have any attributes or methods.
     """
+    pass
+
