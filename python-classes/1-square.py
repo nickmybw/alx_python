57614ae26b5e973fcb054bea42f949202405850e
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-04 15:24:31 +0300

    Updated 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,4 @@
+"""ALX Task 1 of Python Classes and Objects"""
 class Square:
     def __init__(self, size=0):
         """Initialize a square with the given size"""
@@ -8,36 +9,3 @@ class Square:
         """Getter for the dictionary representation of the square"""
         return {'_Square__size': self.__size}
 
-
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.dict_)
-
-mysquare = Square(89)
-print(type(mysquare))
-print(mysquare.dict_)
-
-mysquare = Square()
-print(type(mysquare))
-print(mysquare.dict_)
-
-try:
-    mysquare = Square("3")
-    print(type(mysquare))
-    print(mysquare.dict_)
-except Exception as e:
-    print(e)
-
-try:
-    mysquare = Square(3.14)
-    print(type(mysquare))
-    print(mysquare.dict_)
-except Exception as e:
-    print(e)
-
-try:
-    mysquare = Square(-89)
-    print(type(mysquare))
-    print(mysquare.dict_)
-except Exception as e:
-    print(e)
