11ac2df71261747e043aa1750f35d249c93a8562
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-04 16:15:18 +0300

    first commit

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+
+    :param obj: The object to be checked.
+    :param a_class: The class to compare the object against.
+    :return: True if the object is exactly an instance of the specified class; otherwise False.
+    """
+    return type(obj) is a_class
+
+
+if __name__ == "__main__":
+    # You can add some test cases here to verify the function's functionality.
+    pass
