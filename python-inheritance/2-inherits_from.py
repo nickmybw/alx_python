6638d567001598b3f4aee2688ac0997734d2ac56
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-04 16:26:05 +0300

    updated

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,9 +6,9 @@ def inherits_from(obj, a_class):
 
     :param obj: The object to be checked.
     :param a_class: The class to compare the object against.
-    :return: True if the object is an instance of a subclass of the specified class; otherwise False.
+    :return: True if the object is an instance of a class that inherited from the specified class; otherwise False.
     """
-    return isinstance(obj, type) and issubclass(obj, a_class) and obj is not a_class
+    return isinstance(obj, type) and issubclass(obj, a_class) and obj != a_class
 
 
 if __name__ == "__main__":
