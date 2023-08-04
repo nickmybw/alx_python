a68593a8564fc8db377e6c690befd7c00f4936d3
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-04 15:19:36 +0300

    Updated

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -10,16 +10,3 @@ class Square:
         self.__size = size
 
 
-my_square = Square(3)
-print(type(my_square))  # <class '__main__.Square'>
-print(my_square.__dict__)  # {'_Square__size': 3}
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)  # 'Square' object has no attribute 'size'
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)  # 'Square' object has no attribute '__size'
