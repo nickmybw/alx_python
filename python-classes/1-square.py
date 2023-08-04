755c398803fbbb007a37128e756fcf8881d5906a
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-04 15:36:44 +0300

    updated

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,37 @@
 """ALX Task 1 of Python Classes and Objects"""
+
+
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+
+    Methods:
+        __init__(self, size=0):
+            Constructor for the Square class.
+            Args:
+                size (int, optional): The size of the square. Defaults to 0.
+            Raises:
+                TypeError: If size is not an integer.
+                ValueError: If size is less than 0.
+    """
+
     def __init__(self, size=0):
-        """Initialize a square with the given size"""
+        """
+        Constructor for the Square class.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
-    @property
-    def dict_(self):
-        """Getter for the dictionary representation of the square"""
-        return {'_Square__size': self.__size}
 
