6d5be4b08fe17a0e5403fa9ace241d1c86af917a
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-04 16:22:25 +0300

    first commit

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,16 @@
+"""ALX Python Inheritance Task 2"""
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+
+    :param obj: The object to be checked.
+    :param a_class: The class to compare the object against.
+    :return: True if the object is an instance of a subclass of the specified class; otherwise False.
+    """
+    return isinstance(obj, type) and issubclass(obj, a_class) and obj is not a_class
+
+
+if __name__ == "__main__":
+    # You can add some test cases here to verify the function's functionality.
+    pass
