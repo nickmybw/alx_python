9e8251b0f2cde10c22012d35952ee4825496d793
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-04 15:39:34 +0300

    updated 1

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,28 +1,49 @@
+"""ALX Python Classes and Objects Task 2"""
+
+
 class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private).
+
+    Methods:
+        __init__(self, size=0):
+            Constructor for the Square class.
+            Args:
+                size (int, optional): The size of the square. Defaults to 0.
+            Raises:
+                TypeError: If size is not an integer.
+                ValueError: If size is less than 0.
+
+        area(self):
+            Calculates the area of the square.
+            Returns:
+                int: The area of the square.
+    """
+
     def __init__(self, size=0):
+        """
+        Constructor for the Square class.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
-
-
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
