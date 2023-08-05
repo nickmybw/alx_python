3cc513ff0eec74148e0c6f5a0ee41d9f6e495cac
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-05 14:11:45 +0300

    updated commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,4 +6,9 @@ class BaseGeometry:
     This class will be used as a base class for other geometry-related classes.
     It does not contain any methods or attributes at the moment.
     """
-    pass
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__int_subclass__"]
+        return new_attribute_list
+    
+    
