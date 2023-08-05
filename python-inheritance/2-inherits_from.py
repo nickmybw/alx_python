ec139adf8a112aa4f1b41a1f3971c332f4cb0ef2
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-05 13:40:08 +0300

    first update

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,8 +1,11 @@
 """ALX task 2"""
 def inherits_from(obj, a_class):
+
     """Check if obj is an instance of class."""
+
     obj_class = type(obj)
     if issubclass(obj_class, a_class) and obj_class is not a_class:
         return True
     else:
         return False
+    
