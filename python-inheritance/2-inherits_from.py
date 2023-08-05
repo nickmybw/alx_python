89a13392ea3c3b672ae5aa0516a1956f25cde4be
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-05 13:52:03 +0300

    updated

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,6 +6,6 @@ def inherits_from(obj, a_class):
     obj_class = type(obj)
     if issubclass(obj_class, a_class) and obj_class is not a_class:
         return True
+
     else:
         return False
-    
