263ad183e5898d70e057ca25368fe7670ab81a49
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-05 14:15:45 +0300

    update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
     """
     def __dir__(self):
         attributes = super().__dir__()
-        new_attribute_list = [item for item in attributes if item != "__int_subclass__"]
+        new_attribute_list = [item for item in attributes if item != '__int_subclass__']
         return new_attribute_list
     
     
