a4acc178118d2b2cf4f692d909042372bf38d9d2
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-05 14:34:15 +0300

    updated commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,14 @@
 """Alx task 3"""
-class BaseGeometry:
+
+class BaseGeometryMeta(type):
+    """BaseGeometryMeta description"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != '__init_subclass__']
+        return new_attribute_list
+
+
+class BaseGeometry(metaclass = BaseGeometryMeta):
     """
     Empty class representing the base geometry.
 
