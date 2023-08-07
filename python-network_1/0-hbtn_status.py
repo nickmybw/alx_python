58b23c32b865469eeeed6a03353f93641b3decaf
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-08 02:53:57 +0300

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,40 +1,22 @@
 """
-This module fetches the status of a specified URL using the requests module.
-
-Usage:
-    python3 0-hbtn_status.py
+Module to fetch and display the status from a URL using requests.
 """
 
 import requests
 
 
-def fetch_status(url):
+def fetch_status():
     """
-    Sends a GET request to the specified URL and displays the body of the response.
-
-    Args:
-        url (str): The URL to fetch the status from.
-
-    Returns:
-        str: The body of the response.
+    Fetches and displays the status from the specified URL.
     """
+    url = "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
-    return response.text
+    content = response.text
 
-
-def main():
-    """
-    Main function that fetches the status of two URLs and displays the output.
-    """
-    url1 = "https://intranet.hbtn.io/status"
-    url2 = "http://0.0.0.0:5050/status"
     print("Body response:")
-    print("\t- type: {}".format(type(fetch_status(url1))))
-    print("\t- content: {}".format(fetch_status(url1)))
-    print("Body response:")
-    print("\t- type: {}".format(type(fetch_status(url2))))
-    print("\t- content: {}".format(fetch_status(url2)))
+    print("\t- type:", type(content))
+    print("\t- content:", content)
 
 
 if __name__ == "__main__":
-    main()
+    fetch_status()
