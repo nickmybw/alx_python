8b990a7ab51e9ce3fa2e4ba3a9722d2be39a997e
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-08 02:24:21 +0300

    first commit

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+"""ALX Task 9"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """
+    Represents a square with a side length, x-coordinate, y-coordinate, and unique ID.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a Square object with the given side length, x-coordinate, y-coordinate, and ID.
+
+        Args:
+            size (int): The side length of the square.
+            x (int, optional): The x-coordinate of the square. Defaults to 0.
+            y (int, optional): The y-coordinate of the square. Defaults to 0.
+            id (int, optional): The unique ID of the square. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+
+        Returns:
+            str: A formatted string representing the square.
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
