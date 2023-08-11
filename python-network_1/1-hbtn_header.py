481f76208ae8cd1849eaeaad05219e7d18ff71f3
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-11 11:39:49 +0300

    first commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,23 +4,10 @@ Module to fetch and display the value of the X-Request-Id header from a URL usin
 
 import requests
 import sys
-
-
-def fetch_x_request_id(url):
-    """
-    Fetches and displays the value of the X-Request-Id header from the response.
-    """
-    response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-
-    if x_request_id:
-        print(x_request_id)
-
-
+"""
+Sends a request to a URL and displays the value of the variable X-Request-Id in the response header
+"""
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    fetch_x_request_id(url)
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
