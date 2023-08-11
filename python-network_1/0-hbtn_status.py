5c6b453463b76f8161cc1d91bf5624f6b14ca002
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-11 11:35:25 +0300

    first commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,20 +3,13 @@ Module to fetch and display the status from a URL using requests.
 """
 
 import requests
-
-
-def fetch_status():
-    """
-    Fetches and displays the status from the specified URL.
-    """
-    url = "https://alu-intranet.hbtn.io/status"
-    response = requests.get(url)
-    content = response.text
-
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-
+"""
+Fetches https://alu-intranet.hbtn.io/status using requests package
+"""
 
 if __name__ == "__main__":
-    fetch_status()
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
