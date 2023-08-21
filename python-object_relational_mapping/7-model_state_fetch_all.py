a0ef738e483e4b5acdecb6191793030619b780f1
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-22 00:25:33 +0300

    updated commit

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 """
-Module documentation: This script lists all State objects from the database.
+Module documentation: This script connects to a MySQL database and retrieves
+all State objects from the 'states' table using SQLAlchemy ORM.
 """
 
 import sys
@@ -7,39 +8,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
-def fetch_all_states(username, password, db_name):
-    """
-    Function documentation: Connects to the MySQL database and retrieves all State objects.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        db_name (str): Database name.
-
-    Returns:
-        None.
-    """
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, db_name), pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     states = session.query(State).order_by(State.id).all()
-
     for state in states:
         print("{}: {}".format(state.id, state.name))
-
-    session.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    fetch_all_states(username, password, db_name)
