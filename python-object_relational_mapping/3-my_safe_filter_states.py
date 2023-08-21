1dc84808ab5b35f9100b5462e6d91d8cf68aaaee
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-21 15:38:09 +0300

    first commit

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,56 @@
+"""aLX python object relational mapping task 3
+Module documentation: This script connects to a MySQL database and retrieves
+values from the 'states' table based on the provided state name in a safe way.
+"""
+
+import sys
+import MySQLdb
+
+def filter_states_by_name(username, password, db_name, state_name):
+    """
+    Function documentation: Connects to the MySQL database and retrieves values
+    from the 'states' table based on the provided state name in a safe way.
+
+    Args:
+        username(str): MySQL username.
+        password(str): MySQL password.
+        db_name(str): Database name.
+        state_name(str): State name to search for .
+
+    Returns:
+        None.
+    """
+    try:
+        # Connect to the database
+        db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                             passwd=password, db=db_name)
+
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
+
+        # Create the SQL query with user input using parameterized query
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        cursor.execute(query, (state_name,))
+
+        # Fetch all rows and display them
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+    finally:
+        if db:
+            db.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    filter_states_by_name(username, password, db_name, state_name)
