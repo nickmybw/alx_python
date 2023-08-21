403621c5b84490b511eea65786a9ce0db99f6fdd
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-22 00:34:33 +0300

    update new

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Module documentation: This script connects to a MySQL database and retrieves
 all State objects from the 'states' table using SQLAlchemy ORM.
@@ -9,6 +10,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
