b63f487e81abdfcf7a0604610a6b3c5ff010c72c
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-21 18:21:48 +0300

    new update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,7 @@
-"""Module that contains the class definition of a
-State and an instance Base = declarative_base()
+"""
+Module documentation: This script defines the State class using SQLAlchemy ORM.
 """
 
-import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,17 +9,10 @@ Base = declarative_base()
 
 
 class State(Base):
-    """State class that inherits from Base and links to the MySQL table states
+    """
+    Class documentation: This class represents the 'states'
+    table in the database.
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
