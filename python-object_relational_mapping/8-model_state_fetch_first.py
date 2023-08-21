5c16d651af1ba53464a882349b1c43089bdf790c
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-21 18:08:58 +0300

    new commit

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,5 @@
 """
-Module documentation: This script prints the
-first State object from the database.
+Script that prints the first State object from the database hbtn_0e_6_usa
 """
 
 import sys
@@ -8,44 +7,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+if __name__ == '__main__':
+    # Create SQLAlchemy engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-def fetch_first_state(username, password, db_name):
-    """
-    Function documentation: Connects to the MySQL database
-    and retrieves the first State object.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        db_name (str): Database name.
-
-    Returns:
-        None.
-    """
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password,
-                                                    db_name),
-          pool_pre_ping=True)
+    # Create session factory bound to the engine
     Session = sessionmaker(bind=engine)
+
+    # Create a session instance
     session = Session()
 
-    first_state = session.query(State).order_by(State.id).first()
+    # Query the State table and retrieve the first result
+    result = session.query(State).order_by(State.id).first()
 
-    if first_state is not None:
-        print("{}: {}".format(first_state.id, first_state.name))
+    # Print the result or "Nothing" if the table is empty
+    if result is not None:
+        print("{}: {}".format(result.id, result.name))
     else:
         print("Nothing")
-
-    session.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    fetch_first_state(username, password, db_name)
