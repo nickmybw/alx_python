a25761f75daff7cb3e96db12648c7c284c9cc5a9
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-21 14:46:24 +0300

    update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,38 @@
 """aLX python object relational mapping task 1"""
 import MySQLdb
+import sys
 
 
 def filter_states(username, password, database):
     """
-    Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+    Lists all states from the database hbtn_0e_0_usa with a name starting with N (upper N).
 
     Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
+        username (str): The MySQL username.
+        password (str): The MySQL password.
+        database (str): The name of the database.
 
     Returns:
         None
+
+    Raises:
+        MySQLdb.Error: If there is an error executing the query.
+
     """
     # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database)
 
     # Create a cursor object
     cursor = db.cursor()
 
-    # Execute the SQL query
-    query = "SELECT * FROM hbtn_0e_0_usa WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
+    # Execute the query
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch and display the results
+    # Fetch the results
     results = cursor.fetchall()
+
+    # Display the results
     for row in results:
         print(row)
 
@@ -35,5 +41,6 @@ def filter_states(username, password, database):
     db.close()
 
 
-if __name__ == "__main__":
-    filter_states("your_username", "your_password", "your_database")
+if __name__ == '__main__':
+    # Call the filter_states function with the command line arguments
+    filter_states(sys.argv[1], sys.argv[2], sys.argv[3])
