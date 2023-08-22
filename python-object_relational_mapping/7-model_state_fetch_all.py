aa52615301d16607cc743d3ba9a9f337aa055d88
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-22 13:38:23 +0300

    update

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,25 @@
 """
-Module documentation: This script connects to a MySQL database and retrieves
-all State objects from the 'states' table using SQLAlchemy ORM.
+lists all State objects from the database hbtn_0e_6_usa
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == "__main__":
+    # Set up connection to the database
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(user, password, db_name), pool_pre_ping=True)
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
+    # Query the database and print results
+    query = session.query(State).order_by(State.id)
+    for state in query.all():
         print("{}: {}".format(state.id, state.name))
