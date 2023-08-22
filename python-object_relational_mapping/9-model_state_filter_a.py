87fd2905e67882924332d8fefa579ad77070fed5
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-08-22 14:14:18 +0300

    update

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
-"""
-Lists all State objects that contain the letter a from the database hbtn_0e_6_usa
-"""
+"""Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa"""
+
 
 import sys
 from sqlalchemy import create_engine
@@ -8,24 +8,25 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    # Check for correct number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
 
-    # Create engine and session
-    engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}")
+    # Set up the connection to the database
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query for states containing 'a' and sort by id
-    states = session.query(State).filter(
+    # Query and display results
+    states_with_a = session.query(State).filter(
         State.name.like('%a%')).order_by(State.id).all()
+    for state in states_with_a:
+        print("{}: {}".format(state.id, state.name))
 
-    # Print results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    # Close session
     session.close()
