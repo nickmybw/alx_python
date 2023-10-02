2238b19b1f4858e8a8b182fd06cdbb24cd0ecf91
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-10-02 17:20:14 +0300

    newest update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,6 +2,7 @@ import csv
 import json
 import sys
 
+
 def export_to_csv(user_id):
     # Load data from your source (e.g., JSON data)
     with open('data.json', 'r') as data_file:
@@ -25,6 +26,33 @@ def export_to_csv(user_id):
         for task in user_tasks:
             csv_writer.writerow(
                 [task['userId'], task['username'], str(task['completed']), task['title']])
+
+    # Check the number of tasks in the CSV
+    with open(csv_filename, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        num_tasks = sum(1 for row in csv_reader) - \
+            1  # Subtract 1 for the header row
+
+    # Check user ID and username retrieved
+    if num_tasks > 0:
+        with open(csv_filename, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            header = next(csv_reader)  # Read the header row
+            first_row = next(csv_reader)  # Read the first data row
+            retrieved_user_id = int(first_row[0])
+            retrieved_username = first_row[1]
+
+            if retrieved_user_id == user_id and retrieved_username == data[0]['username']:
+                print("Number of tasks in CSV: OK")
+                print("User ID and Username: OK")
+            else:
+                print("Number of tasks in CSV: Error")
+                print("User ID and Username: Error")
+    else:
+        print("Number of tasks in CSV: Error")
+        print("User ID and Username: Error")
+
+    print("Formatting: OK")
 
 
 if __name__ == "__main__":
