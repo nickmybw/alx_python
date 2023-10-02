3e4da409f174a3859b093666b2629d76bd46dc25
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-10-02 17:38:19 +0300

    updated

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,51 +1,43 @@
-import csv
+"""
+Gather data from an API
+"""
 import requests
 import sys
-import cvs
+import csv
 
 
-def fetch_employee_data(employee_id):
-    # Define the API endpoints for the employee and their TODO list
-    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit("Usage: ./0-gather_data_from_an_API.py <employee_id>")
 
-    # Fetch data from the API
-    employee_response = requests.get(employee_url)
+    employee_id = sys.argv[1]
+
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
+        employee_id)
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        employee_id)
+
+    user_response = requests.get(user_url)
     todo_response = requests.get(todo_url)
 
-    # Check if the requests were successful
-    if employee_response.status_code != 200:
-        print("Error: Unable to fetch employee data.")
-        sys.exit(1)
+    if user_response.status_code != 200:
+        sys.exit("Error: User data not found")
+
     if todo_response.status_code != 200:
-        print("Error: Unable to fetch TODO list data.")
-        sys.exit(1)
+        sys.exit("Error: TODO data not found")
 
-    # Parse JSON responses
-    employee_data = employee_response.json()
-    todo_list = todo_response.json()
+    user_data = user_response.json()
+    todo_data = todo_response.json()
 
-    return employee_data, todo_list
+    employee_name = user_data.get("name")
+    total_tasks = len(todo_data)
+    completed_tasks = [task for task in todo_data if task.get("completed")]
 
-
-def main():
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-
-    employee_data, todo_list = fetch_employee_data(employee_id)
-
-    employee_name = employee_data.get('name')
-    completed_tasks = [task for task in todo_list if task['completed']]
-    total_tasks = len(todo_list)
-
-    print(
-        f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(completed_tasks), total_tasks))
 
     for task in completed_tasks:
-        print(f"\t{task['title']}")
+        print("\t {}".format(task.get("title")))
 
 
 def export_to_csv(employee_id, employee_name, todo_list):
