5c3545b7646fe4ac780d2bcbb2c0c40ddb0fa65f
nick_mybw <101449835+nickmybw@users.noreply.github.com>
2023-10-02 18:00:48 +0300

    new

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,43 +2,39 @@ import requests
 import sys
 
 
-def get_employee_data(employee_id):
-    # Define the base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com/"
-
-    # Make a GET request to fetch employee details
-    employee_response = requests.get(f"{base_url}users/{employee_id}")
-    employee_data = employee_response.json()
-
-    # Make a GET request to fetch employee's TODO list
-    todo_response = requests.get(f"{base_url}users/{employee_id}/todos")
-    todo_list = todo_response.json()
-
-    return employee_data, todo_list
-
-
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+        return
 
-    employee_id = int(sys.argv[1])
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    # Fetch employee data and TODO list
-    employee_data, todo_list = get_employee_data(employee_id)
+    try:
+        # Fetch employee details
+        employee_response = requests.get(f"{base_url}/users/{employee_id}")
+        employee_data = employee_response.json()
+        employee_name = employee_data.get("name")
 
-    # Extract relevant information
-    employee_name = employee_data["name"]
-    total_tasks = len(todo_list)
-    completed_tasks = sum(1 for task in todo_list if task["completed"])
-    completed_task_titles = [task["title"]
-                             for task in todo_list if task["completed"]]
+        # Fetch employee's TODO list
+        todos_response = requests.get(f"{base_url}/users/{employee_id}/todos")
+        todos_data = todos_response.json()
 
-    # Display the information in the specified format
-    print(
-        f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-    for title in completed_task_titles:
-        print(f"\t{title}")
+        # Calculate completed and total tasks
+        total_tasks = len(todos_data)
+        completed_tasks = sum(
+            1 for task in todos_data if task.get("completed"))
+
+        # Display the result
+        print(
+            f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+        for task in todos_data:
+            if task.get("completed"):
+                print(f"\t{task.get('title')}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return
 
 
 if __name__ == "__main__":
